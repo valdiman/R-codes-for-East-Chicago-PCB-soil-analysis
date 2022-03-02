3302c015265fa30dc12aadd3a1ae2ea69498c1b7
Andres Martinez <andres-martinez@uiowa.edu>
2022-03-02 12:33:16 -0600

    Initial ECSoilStatTest script

--- test.R
+++ test.R
@@ -1,1 +1,9 @@
-library(tidyr)
+# Anova -------------------------------------------------------------------
+# For total PCB EC and other locations
+
+# Here I'm using data from my folder.
+# Once the dataset in Pangaea is ready, this need to be updated
+
+# set working directory
+setwd(paste("/Users/andres/OneDrive - University of Iowa/work",
+            "/ISRP/Project6/Soil/Data", sep = ""))
